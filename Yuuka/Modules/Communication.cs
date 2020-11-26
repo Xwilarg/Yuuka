40ee3ae7bfe8d12a4e193be7595a5c49c921579c
Xwilarg <xwilarg@yahoo.fr>
2020-11-26 02:56:53 +0100

    Improve BotInfo command

--- Yuuka/Modules/Communication.cs
+++ Yuuka/Modules/Communication.cs
@@ -1,5 +1,10 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 using DiscordUtils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Yuuka.Modules
@@ -9,7 +14,54 @@ namespace Yuuka.Modules
         [Command("BotInfo")]
         public async Task Info()
         {
-            await ReplyAsync(embed: Utils.GetBotInfo(Program.P.StartTime, "Yuuka", Program.P.Client.CurrentUser));
+            await ReplyAsync(embed: new EmbedBuilder()
+            {
+                Color = Color.Purple,
+                Fields = new List<EmbedFieldBuilder>
+                {
+                    new EmbedFieldBuilder
+                    {
+                        Name = "Uptime",
+                        Value = Utils.TimeSpanToString(DateTime.Now.Subtract(Program.P.StartTime)),
+                        IsInline = true
+                    },
+                    new EmbedFieldBuilder
+                    {
+                        Name = "Creator",
+                        Value = "Zirk#0001",
+                        IsInline = true
+                    },
+                    new EmbedFieldBuilder
+                    {
+                        Name = "Account creation",
+                        Value = Program.P.Client.CurrentUser.CreatedAt.ToString("HH:mm:ss dd/MM/yy"),
+                        IsInline = true
+                    },
+                    new EmbedFieldBuilder
+                    {
+                        Name = "Last version",
+                        Value = new FileInfo(Assembly.GetEntryAssembly().Location).LastWriteTimeUtc.ToString("HH:mm:ss dd/MM/yy"),
+                        IsInline = true
+                    },
+                    new EmbedFieldBuilder
+                    {
+                        Name = "Guild Count",
+                        Value = Program.P.Client.Guilds.Count,
+                        IsInline = true
+                    },
+                    new EmbedFieldBuilder
+                    {
+                        Name = "GitHub",
+                        Value = "https://github.com/Xwilarg/Yuuka",
+                        IsInline = true
+                    },
+                    new EmbedFieldBuilder
+                    {
+                        Name = "top.gg Page",
+                        Value = "https://top.gg/bot/734788725388869742",
+                    }
+                }
+            }.Build());
         }
     }
 }
